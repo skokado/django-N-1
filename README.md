2ab6c994463647dbec962b90350340ad69519148
skokado <terutacchi@gmail.com>
2021-10-08 15:11:12 +0900

    ダミーデータ作成コマンドを実装

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 参考 https://selfs-ryo.com/detail/django_nplusone
+
+
+```shell
+$ python3 manage.py migrate
+$ # ランダムデータを10万件登録
+$ python3 manage.py gendata
+```
---
+++ app/management/commands/gendata.py
@@ -0,0 +1,45 @@
+import random
+
+from django.core.management.base import BaseCommand, CommandError
+from faker import Faker
+from tqdm import tqdm
+
+from app import models
+
+faker = Faker()
+DEFAULT_POSTS_RECORDS = 100000
+
+CATEGORY_CHOICES = (
+    'プログラミング',
+    'デザイン',
+    'マネジメント',
+    'ビジネス',
+    'その他',
+)
+
+class Command(BaseCommand):
+    help = 'Generate random datas using faker'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--n', nargs='+', type=int, default=DEFAULT_POSTS_RECORDS)
+
+    def handle(self, *args, **options):
+        n: int = options['n']
+
+        # カテゴリ登録
+        categories = []
+        for category in CATEGORY_CHOICES:
+            categories.append(models.Category(
+                name=category
+            ))
+        models.Category.objects.bulk_create(categories, ignore_conflicts=True)
+
+        posts = [
+            models.Post(
+                title=faker.text(),
+                content=' '.join(faker.texts()),
+                category_id=random.choice(range(1, len(CATEGORY_CHOICES) + 1))
+            )
+            for _ in tqdm(range(n))
+        ]
+        models.Post.objects.bulk_create(posts)
