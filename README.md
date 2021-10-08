cb91e0da721ffca85832022cad10824c5399d63e
skokado <terutacchi@gmail.com>
2021-10-08 15:09:49 +0900

    プロジェクト初期化

---
+++ README.md
@@ -0,0 +1,1 @@
+参考 https://selfs-ryo.com/detail/django_nplusone
---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField('カテゴリ名', max_length=255, unique=True)
+
+
+class Post(models.Model):
+    title = models.CharField('タイトル', max_length=255)
+    content = models.TextField('本文')
+    category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
