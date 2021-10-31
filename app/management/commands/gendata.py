270b4d0281a5cce1c332b59fda5d8eb9b0f4f022
skokado <terutacchi@gmail.com>
2021-11-01 01:00:52 +0900

    多対多リレーションの例を記載

--- app/management/commands/gendata.py
+++ app/management/commands/gendata.py
@@ -17,6 +17,22 @@ CATEGORY_CHOICES = (
     'その他',
 )
 
+
+TAGS = (
+    models.Tag.objects.create(name='Python'),
+    models.Tag.objects.create(name='Ruby'),
+    models.Tag.objects.create(name='Go'),
+    models.Tag.objects.create(name='Kotlin'),
+)
+
+# TAGS = (
+#     'Python',
+#     'Ruby',
+#     'Go',
+#     'Kotlin',
+# )
+
+
 class Command(BaseCommand):
     help = 'Generate random datas using faker'
 
@@ -38,9 +54,15 @@ class Command(BaseCommand):
             models.Post(
                 title=faker.text(),
                 content=' '.join(faker.texts()),
-                category_id=random.choice(range(1, len(CATEGORY_CHOICES) + 1))
+                category_id=random.choice(range(1, len(CATEGORY_CHOICES) + 1)),
             )
             for _ in tqdm(range(n))
         ]
         models.Post.objects.bulk_create(posts)
+        posts = models.Post.objects.all()
+        # 3個のタグを登録する
+        for post in posts:
+            tags = random.sample(TAGS, 3)
+            post.tags.add(*tags)
+            
         print(f'Generated {n} post records')
